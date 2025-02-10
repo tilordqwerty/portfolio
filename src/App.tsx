2eca57a72c6dc2010d2fe00dc88d615265b77685
tilordqwerty <elgabrielcisneros@gmail.com>
2025-02-10 18:13:35 -0400

    Cambiado titulo en index.html

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import './App.css';
 import './index.css';
+import './fonts/Lora/Lora-Bold.ttf';  
 
 function App() {
 
   return (
     <>
-      <h1 className='mb-b'>
-        Hello World
+      <h1 className='justify-center container mx-auto text-4xl text-center font-lora'>
+        Hi, I am Gabriel Cisneros 👋
       </h1>
+      <h2 className='headerComponent justify-center container mx-auto text-3xl text-center font-lora'>
+        Software Developer
+      </h2>
+      <button className='box-border w-24 h-12 m-4 p-2 text-lg text-white bg-blue-500 rounded-md hover:bg-blue-700'>
+        Click me
+      </button>
     </>
   )
 }
